310e447152591389b5b5688112d0eb659e51a817
Vardan <20cs1a1025@kristujayanti.com>
2023-03-19 18:53:01 +0530

    watch-video-component complete

--- src/app/watch-video/watch-video.component.ts
+++ src/app/watch-video/watch-video.component.ts
@@ -4,6 +4,7 @@ import { UserService } from '../user.service';
 import { VideoDto } from '../video-dto';
 import { VideoService } from '../video.service';
 import { Subscription } from 'rxjs';
+import { UserDto } from '../user-dto';
 
 @Component({
   selector: 'app-watch-video',
@@ -14,35 +15,44 @@ export class WatchVideoComponent implements OnInit{
 
   videoId!: string;
   videoData = {} as VideoDto;
+  userData = {} as UserDto;
   videoAvailable: boolean = false;
   showSubscribeButton: boolean = true;
   showUnSubscribeButton: boolean = false;
   paramSubscription!: Subscription;
+  formattedSubCount=0
 
   constructor(private activatedRoute: ActivatedRoute, private videoService: VideoService, private userService: UserService) {
     
-      // this.title.setValue(this.videoData.videoName);
-      // this.description.setValue(this.videoData.description);
-      // this.videoStatus.setValue(this.videoData.videoStatus);
-      var x = this.activatedRoute.snapshot.params['videoId'];
-      if(x === null){
-        console.warn("Null videoId")
-        //REDIRECT_ERROR
-      }
-      else{
-        this.videoId=x;
-        this.videoService.getVideo(x).subscribe(data => {
-          this.videoData=data;
-          if(this.videoData.videoId === null){
-            console.warn("Wrong videoId")
-            //REDIRECT_ERROR
-          }
-          else{
-            this.videoAvailable = true;            
-          }
-        });
-        
-      }
+    var x = this.activatedRoute.snapshot.params['videoId'];
+    if(x === null){
+      console.warn("Null videoId")
+      //REDIRECT_ERROR
+    }
+    else{
+      this.videoId=x;
+      this.videoService.getVideo(x).subscribe(data => {
+        this.videoData=data;
+        if(this.videoData.videoId === null){
+          console.warn("Wrong videoId")
+          //REDIRECT_ERROR
+        }
+        else{
+          this.videoAvailable = true;            
+        }
+      });
+
+      //TestUserData
+      this.userData.id="testId";
+      this.userData.sub="testSub";
+      this.userData.firstName="Vishnu";
+      this.userData.lastName="Vardan";
+      this.userData.fullName="Vishnu Vardan";
+      this.userData.email="vishnuvs7619@kristujayanti.com";
+      this.userData.profilePicUrl="https://lh3.googleusercontent.com/a/AGNmyxZFgBjFshpxqJ9SnjN5KAzD8YskwF4d5Cofzro-KQ=s360";
+      this.userData.subscribersCount=1395634;
+      
+    }
   }
   ngOnInit(): void {
     // var x: string | null = null;
@@ -72,6 +82,22 @@ export class WatchVideoComponent implements OnInit{
     
   }
 
+  formatNumber(num: number): string {
+    var s = "0";
+    if (num < 1000) {
+      s = num.toString();
+    } else if (num < 1000000) {
+      s = (num / 1000).toFixed(1) + "K";
+    } else if (num < 1000000000) {
+      s = (num / 1000000).toFixed(1) + "M";
+    } else if (num < 1000000000000) {
+      s = (num / 1000000000).toFixed(1) + "B";
+    } else {
+      s = (num / 1000000000000).toFixed(1) + "T";
+    }
+
+    return s + " subscribers";
+  }
 
   loadVideoDetails(){
     
